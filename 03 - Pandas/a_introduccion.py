7e0114b648cf027a1cd2988cd788252ea5e72f86
Francis Ferri <francis.ferri@epn.edu.ec>
2020-07-17 17:17:32 -0500

    Pandas introduccion

---
+++ 03 - Pandas/a_introduccion.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 16:29:38 2020
+
+@author: franc
+"""
+
+
+# Los aarchivos de python no puedeneempesar con numeros. 
+#Un archivo de python es como una variable para el interprete
+
+print("Hola Spider") # Selecciona la parte del 
+#codigo que quieres ejecutar y dale  shitf + enter
+
+nombre = "Adrian"
+numero = 3
+lista = [1,2,3]
+tupla = (1,2,3)
+diccionario = {
+    "nombre": "Francis",
+    "apellido": "Ferri"
+    }
