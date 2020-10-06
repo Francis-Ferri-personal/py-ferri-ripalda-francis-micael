16fb6fc8f90a8d47f412afc8d4197c7406387fd6
Francis Ferri <francis.ferri@epn.edu.ec>
2020-10-06 15:24:28 -0500

    Spider de libros

--- 04 - scrapy/03-intro-spider/arania_basica/arania_basica/spiders/arania_ejemplo.py
+++ 04 - scrapy/03-intro-spider/arania_basica/arania_basica/spiders/arania_ejemplo.py
@@ -14,9 +14,44 @@ class IntroSCrapy(scrapy.Spider):
         etiqueta_contenedora = response.css(
             'article.product_pod'
         )
+
+        urlsImagen = etiqueta_contenedora.css(
+            ".image_container > a > img::attr(src)"
+        ).extract()
+        print("IMAGENES")
+        print(urlsImagen)
+        
+        starRatings = etiqueta_contenedora.css(
+            ".star-rating::attr(class)"
+        ).extract()
+        stars = list(map(lambda starRating: starRating.split()[1], starRatings))
+        print("STARS")
+        print(stars)
+
         titulos = etiqueta_contenedora.css(
             'h3 > a::text'
         ).extract()
+        print("TITULOS")
         print(titulos)
+        
+        prices = etiqueta_contenedora.css(
+            ".product_price > .price_color::text"
+        ).extract()
+        prices = list(map(lambda precio: float(precio[1:]), prices))
+        print("PRICE")
+        print(prices)
+        
+        stocks = etiqueta_contenedora.css(
+            ".product_price > .instock::text"
+        ).extract()
+        stocks = list(map(lambda stock: stock.strip(), stocks))
+        stocks = list(filter(lambda stock: stock != '', stocks))
+        print("STOCKS")
+        print(stocks)
+
+        
+            
+             
+
 
 # scrapy crawl introduccion_spider
